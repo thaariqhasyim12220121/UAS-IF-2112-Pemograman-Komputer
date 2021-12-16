f6ba3eb39d574b5d38dd6364ce63f48d171ef085
thaariqhasyim12220121 <96248930+thaariqhasyim12220121@users.noreply.github.com>
2021-12-17 05:01:07 +0700

    Update soal1b.py

--- apps/soal1b.py
+++ apps/soal1b.py
@@ -15,23 +15,28 @@ import json
 
 def app():
     
-    excel_file = 'produksi_minyak_mentah.csv'
+    excel_file = 'produksi_minyak_mentah.csv' 
 
-    with open('kode_negara_lengkap.json') as f:
-            data = json.load(f)
-            names = dict()
-            for x in data:
-                country = x['name']
-                idc = x['alpha-3']
-                names[idc] = country
+    df = pd.read_csv(excel_file)
+
+    #OPEN JSON
+    with open("kode_negara_lengkap.json") as f:
+        data = json.load(f)
+
+    #MENGGANTI KODE NEGARA DENGAN NAMA NEGARA LENGKAP
+    konversi = {item['alpha-3'] : item['name'] for item in data}
+    df.loc[:, 'kode_negara'] = df['kode_negara'].map(konversi)
+
+    #MENGHILANGKAN DATA NaN
+    df.dropna(subset=["kode_negara"], inplace=True)
 
     st.write("# Tahun Produksi Terbanyak")
 
-    frame3 = pd.read_csv(excel_file)
-    frame3['kode_negara'].replace(to_replace=['AUS', 'AUT', 'BEL', 'CAN', 'CZE', 'DNK', 'FIN', 'FRA', 'DEU', 'GRC', 'HUN', 'ISL', 'IRL', 'ITA', 'JPN', 'KOR', 'LUX', 'MEX', 'NLD', 'NZL', 'NOR', 'POL', 'PRT', 'SVK', 'ESP', 'SWE', 'CHE', 'TUR', 'GBR', 'USA', 'ALB', 'DZA', 'ARG', 'ARM', 'AZE', 'BGD', 'BLR', 'BIH', 'BRA', 'BRN', 'BGR', 'KHM', 'CHL', 'CHN', 'COL', 'HRV', 'CYP', 'EGY', 'EST', 'ETH', 'GEO', 'GHA', 'HTI', 'HKG', 'IND', 'IDN', 'IRN', 'ISR', 'KAZ', 'LVA', 'LTU', 'MKD', 'MYS', 'MLT', 'MDA', 'MOZ', 'NGA', 'PAK', 'PRY', 'PER', 'PHL', 'ROU', 'RUS', 'SAU', 'SGP', 'SVN', 'ZAF', 'SDN', 'TWN', 'TZA', 'THA', 'UKR', 'ARE', 'URY', 'VNM', 'ZMB', 'SRB', 'MNE', 'AGO', 'BHR', 'BEN', 'BOL', 'BWA', 'CMR', 'COG', 'CRI', 'CIV', 'CUB', 'PRK', 'COD', 'DOM', 'ECU', 'SLV', 'ERI', 'GAB', 'GTM', 'HND', 'IRQ', 'JAM', 'JOR', 'KEN', 'KWT', 'KGZ', 'LBN', 'LBY', 'MNG', 'MAR', 'MMR', 'NAM', 'NPL', 'NIC', 'NER', 'OMN', 'PAN', 'QAT', 'SEN', 'LKA', 'SYR', 'TJK', 'TGO', 'TTO', 'TUN', 'TKM', 'UZB', 'VEN', 'YEM', 'ZWE'],value=[names['AUS'], names['AUT'], names['BEL'], names['CAN'], names['CZE'], names['DNK'], names['FIN'], names['FRA'], names['DEU'], names['GRC'], names['HUN'], names['ISL'], names['IRL'], names['ITA'], names['JPN'], names['KOR'], names['LUX'], names['MEX'], names['NLD'], names['NZL'], names['NOR'], names['POL'], names['PRT'], names['SVK'], names['ESP'], names['SWE'], names['CHE'], names['TUR'], names['GBR'], names['USA'], names['ALB'], names['DZA'], names['ARG'], names['ARM'], names['AZE'], names['BGD'], names['BLR'], names['BIH'], names['BRA'], names['BRN'], names['BGR'], names['KHM'], names['CHL'], names['CHN'], names['COL'], names['HRV'], names['CYP'], names['EGY'], names['EST'], names['ETH'], names['GEO'], names['GHA'], names['HTI'], names['HKG'], names['IND'], names['IDN'], names['IRN'], names['ISR'], names['KAZ'], names['LVA'], names['LTU'], names['MKD'], names['MYS'], names['MLT'], names['MDA'], names['MOZ'], names['NGA'], names['PAK'], names['PRY'], names['PER'], names['PHL'], names['ROU'], names['RUS'], names['SAU'], names['SGP'], names['SVN'], names['ZAF'], names['SDN'], names['TWN'], names['TZA'], names['THA'], names['UKR'], names['ARE'], names['URY'], names['VNM'], names['ZMB'], names['SRB'], names['MNE'], names['AGO'], names['BHR'], names['BEN'], names['BOL'], names['BWA'], names['CMR'], names['COG'], names['CRI'], names['CIV'], names['CUB'], names['PRK'], names['COD'], names['DOM'], names['ECU'], names['SLV'], names['ERI'], names['GAB'], names['GTM'], names['HND'], names['IRQ'], names['JAM'], names['JOR'], names['KEN'], names['KWT'], names['KGZ'], names['LBN'], names['LBY'], names['MNG'], names['MAR'], names['MMR'], names['NAM'], names['NPL'], names['NIC'], names['NER'], names['OMN'], names['PAN'], names['QAT'], names['SEN'], names['LKA'], names['SYR'], names['TJK'], names['TGO'], names['TTO'], names['TUN'], names['TKM'], names['UZB'], names['VEN'], names['YEM'], names['ZWE']],inplace=True)
-    tahunminyak = frame3['tahun'].unique().tolist()
+    
+    
+    tahunminyak = df['tahun'].unique().tolist()
     minyaktahun = st.selectbox("Tahun",tahunminyak)
-    df2 = frame3.loc[frame3['tahun'] == minyaktahun].sort_values(
+    df2 = df.loc[df['tahun'] == minyaktahun].sort_values(
     by=['produksi'], ascending=False)
     B1 = int(st.number_input("Banyak Negara", min_value=1, max_value=len(df2)))
     df2 = df2.nlargest(B1, columns='produksi')
